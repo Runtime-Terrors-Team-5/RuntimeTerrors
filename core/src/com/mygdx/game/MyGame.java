82af58602029461d172f1e95a9ad4adb834c9594
KHJH988 <98236360+KHJH988@users.noreply.github.com>
2022-12-07 17:52:53 +0000

    added  WASD player controls for a  player sprite
    Added a player class which's sprite can be controlled using WASD movement via the Update method. The float attribute in the player class is used to control the speed of the sprite. The player class is instantiated in the MyGame class

--- core/src/com/mygdx/game/MyGame.java
+++ core/src/com/mygdx/game/MyGame.java
@@ -1,25 +1,43 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.awt.*;
+
 public class MyGame extends ApplicationAdapter {
-	SpriteBatch batch;
+	SpriteBatch batch; // holds all images
 	Texture img;
+
+	Player player;
+
 	
 	@Override
+	// format for loading assets into game
+	// dropImage = new Texture(Gdx.files.internal("droplet.png"));
+
+	// load the drop sound effect and the rain background "music"
+	//dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+	//rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+	// start the playback of the background music immediately
+    //  rainMusic.setLooping(true);
+    //  rainMusic.play();
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("Owlet_Monster.png");
+		player = new Player(img); // instantiation of player class
+
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		player.Draw(batch);
 		batch.end();
 	}
 	
---
+++ core/src/com/mygdx/game/Player.java
@@ -0,0 +1,42 @@
+package com.mygdx.game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.Input.Keys;
+
+public class Player {
+
+    // attributes
+    public Vector2 position;
+    public Sprite sprite;
+
+    public float speed = 300; // player movement speed
+
+    // methods
+    public Player(Texture img){
+        sprite = new Sprite(img);
+        sprite.setScale(4);
+        position = new Vector2(Gdx.graphics.getWidth()/2,sprite.getScaleY()*sprite.getHeight()/2);
+    }
+
+    public void Update(float deltaTime){
+        // WASD movement input processing
+        if(Gdx.input.isKeyPressed(Keys.A)) position.x-=deltaTime*speed;
+        if(Gdx.input.isKeyPressed(Keys.D)) position.x+=deltaTime*speed;
+        if(Gdx.input.isKeyPressed(Keys.S)) position.y-=deltaTime*speed;
+        if(Gdx.input.isKeyPressed(Keys.W)) position.y+=deltaTime*speed;
+        // stops player from moving out of the window boundaries
+        if(position.x-(sprite.getWidth()*sprite.getScaleX()/2)<=0) position.x = (sprite.getWidth()*sprite.getScaleX()/2);
+        if(position.x+(sprite.getWidth()*sprite.getScaleX()/2)>=Gdx.graphics.getWidth()) position.x = Gdx.graphics.getWidth() - (sprite.getWidth()*sprite.getScaleX()/2);
+        if(position.y-(sprite.getHeight()*sprite.getScaleY()/2)<=0) position.y = (sprite.getHeight()*sprite.getScaleY()/2);
+        if(position.y+(sprite.getHeight()*sprite.getScaleY()/2)>=Gdx.graphics.getHeight()) position.y = Gdx.graphics.getHeight() - (sprite.getHeight()*sprite.getScaleY()/2);
+    }
+
+    public void Draw(SpriteBatch batch){
+        Update(Gdx.graphics.getDeltaTime());
+        sprite.setPosition(position.x, position.y);
+        sprite.draw(batch);
+    }
+}
