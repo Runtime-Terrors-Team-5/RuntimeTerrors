92361473a109490681465c8fc30e047bac7651e2
jguan924 <117920007+jguan924@users.noreply.github.com>
2023-01-20 12:35:09 +0000

    Revert "added a food item class"
    This reverts commit da173a8ceeea06d0c283cde1f324e8e9101f6060.

--- core/src/Screens/Play_Screen.java
+++ core/src/Screens/Play_Screen.java
@@ -18,7 +18,6 @@ import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.Player;
-import com.mygdx.game.foodItems;
 
 public class Play_Screen implements Screen {
     private MyGame game;
@@ -30,14 +29,12 @@ public class Play_Screen implements Screen {
     // sets screen size
     private Viewport game_port;
     private OrthographicCamera gamecam;
-    public TextureAtlas atlas;
+    TextureAtlas atlas;
 
     // imports the kitchen map
     private TmxMapLoader maploader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
-
-    public foodItems items;
 
 
     Texture texture; // temp
@@ -45,7 +42,7 @@ public class Play_Screen implements Screen {
     public Play_Screen(MyGame game){
         this.game = game;
 
-        atlas = new TextureAtlas(Gdx.files.internal("ENG1_Assets_V2.atlas"));
+        atlas = new TextureAtlas(Gdx.files.internal("ENG1_Assets_V1.atlas"));
         chef1 = new Player(atlas.findRegion("C_Blue_N"),atlas.findRegion("M_Blue_N"));
         chef2 = new Player(atlas.findRegion("C_Green_N"),atlas.findRegion("M_Green_N"));
         chef3 = new Player(atlas.findRegion("C_Red_N"),atlas.findRegion("M_Red_N"));
@@ -60,9 +57,6 @@ public class Play_Screen implements Screen {
         renderer = new OrthogonalTiledMapRenderer(map);
         // map camera
         gamecam.position.set(game_port.getWorldWidth(), game_port.getWorldHeight(), 0);
-
-
-        items = new foodItems("E_Onion");
     }
     @Override
     public void show(){
@@ -77,20 +71,10 @@ public class Play_Screen implements Screen {
         game.batch.setProjectionMatrix(gamecam.combined);
         game.batch.begin();
         game.font.draw(game.batch, "The Main game screen", 100, 400);
-
-        game.batch.draw(items.getItemSprite(),0,400);
-        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){
-            items.nextStage();
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){
-            items.fail();
-        }
-
         chef1.Draw(game.batch);
         chef2.Draw(game.batch);
         chef3.Draw(game.batch);
-
-
+        game.batch.draw(chefSelection[chefPointer].sprite,0,400);
 
         // game map
         renderer.render();
