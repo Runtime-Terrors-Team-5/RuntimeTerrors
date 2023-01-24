b07ddebc607f36844543b19586e05ca905fbe3fe
jguan924 <117920007+jguan924@users.noreply.github.com>
2023-01-24 15:16:08 +0000

    some clean up of code
    reduced to 2 chef cleaned way to generate chef and optimised imports

--- core/src/Screens/Play_Screen.java
+++ core/src/Screens/Play_Screen.java
@@ -1,42 +1,33 @@
 package Screens;
+
 import Tools.World_contact_listener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.ScreenUtils;
-
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Screen;
-
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.MyGame;
 import com.mygdx.game.Player;
-import com.mygdx.game.foodItems;
 import org.javatuples.Pair;
 import org.javatuples.Triplet;
-import org.javatuples.Tuple;
 
 import java.util.HashMap;
 
 public class Play_Screen implements Screen {
     private MyGame game;
-    Player chef1;
-    Player chef2;
-    Player chef3;
+
     public static Player[] chefSelection;
     public static int chefPointer;
 
@@ -56,40 +47,29 @@ public class Play_Screen implements Screen {
 
 
 
-    Texture texture; // temp
 
     public Play_Screen(MyGame game){
         this.game = game;
 
         recipes = new HashMap<>();
-        recipes.put("E_Salad",new Triplet<>(new Pair<String, Integer>("E_Lettuce",2),
-                new Pair<String, Integer>("E_Tomato",2),
-                new Pair<String, Integer>("E_Onion",2)));
-        recipes.put("E_Burger",new Triplet<>(new Pair<String, Integer>("E_Lettuce",2),
-                new Pair<String, Integer>("E_Patty",2),
-                new Pair<String, Integer>("E_Bun",1)));
+        recipes.put("E_Salad",
+                new Triplet<>(new Pair<>("E_Lettuce", 2), new Pair<>("E_Tomato", 2), new Pair<>("E_Onion", 2)));
+
+        recipes.put("E_Burger",
+                new Triplet<>(new Pair<>("E_Lettuce", 2), new Pair<>("E_Patty", 2), new Pair<>("E_Bun", 1)));
 
 
         atlas = new TextureAtlas(Gdx.files.internal("ENG1_Assets_V2.atlas"));
-        chef1 = new Player(atlas.findRegion("C_Blue_N"),atlas.findRegion("M_Blue_N"),atlas.findRegion("M_Blue_C123"));
-        chef2 = new Player(atlas.findRegion("C_Green_N"),atlas.findRegion("M_Green_N"),atlas.findRegion("M_Green_C123"));
-        chef3 = new Player(atlas.findRegion("C_Red_N"),atlas.findRegion("M_Red_N"),atlas.findRegion("M_Red_C123"));
-        chefSelection = new Player[]{chef1, chef2, chef3};
+
+        chefSelection = new Player[]{
+                new Player(atlas.findRegion("C_Blue_N"),atlas.findRegion("M_Blue_N"),
+                        atlas.findRegion("M_Blue_C123")),
+
+                new Player(atlas.findRegion("C_Green_N"),atlas.findRegion("M_Green_N"),
+                        atlas.findRegion("M_Green_C123"))
+        };
+
         chefPointer = 0;
-
-/*
-        foodItems temp1 = new foodItems("E_Lettuce");
-        foodItems temp2 = new foodItems("E_Patty");
-        foodItems temp3 = new foodItems("E_Bun");
-        temp1.nextStage();
-        temp2.nextStage();
-        temp2.nextStage();
-        temp3.nextStage();
-        chef1.inventory.addItem(temp1);
-        chef1.inventory.addItem(temp2);
-        chef1.inventory.addItem(temp3);
-*/
-
 
         gamecam = new OrthographicCamera();
         game_port = new FitViewport(MyGame.V_WIDTH,MyGame.V_HEIGHT ,gamecam);
@@ -270,15 +250,12 @@ public class Play_Screen implements Screen {
         game.batch.setProjectionMatrix(gamecam.combined);
         game.batch.begin();
 
-        game.batch.draw(atlas.findRegion("E_Onion",0),0,600);
-        game.batch.draw(atlas.findRegion("E_Onion",1),0,700);
-        game.batch.draw(atlas.findRegion("E_Onion",2),0,800);
-        game.batch.draw(atlas.findRegion("E_Onion",3),0,900);
-
         game.font.draw(game.batch, "The Main game screen", 100, 400);
-        chef1.Draw(game.batch);
-        chef2.Draw(game.batch);
-        chef3.Draw(game.batch);
+
+        for (Player chefs:
+             chefSelection) {
+            chefs.Draw(game.batch);
+        }
         game.batch.draw(chefSelection[chefPointer].sprite,0,400);
         // renderer the box2d lines
         b2dr.render(world, gamecam.combined);
@@ -311,8 +288,9 @@ public class Play_Screen implements Screen {
             chefPointer += 1;
             if (chefPointer>chefSelection.length-1){chefPointer=0;}
         }
+        // test only to be removed
         if (Gdx.input.isKeyJustPressed(Input.Keys.P)){
-            chef1.inventory.stack[2].nextStage();
+            chefSelection[chefPointer].inventory.stack[2].nextStage();
 
         }
         if (Gdx.input.isKeyJustPressed(Input.Keys.C)){
