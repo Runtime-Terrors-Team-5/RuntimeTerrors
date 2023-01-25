e220bb1345dca0532d69183251b205b05e47baf8
KHJH988 <98236360+KHJH988@users.noreply.github.com>
2023-01-25 15:21:15 +0000

    added the service counter and rest so it can be collided with

--- core/src/com/mygdx/game/Cooking_station.java
+++ core/src/com/mygdx/game/Cooking_station.java
@@ -1,4 +1,14 @@
 package com.mygdx.game;
 
-public class Cooking_station {
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Cooking_station extends InteractivetileObject{
+
+    public Cooking_station(World world, TiledMap map, MapObject bounds){
+        super(world, map, bounds);
+        fixture.setUserData(this);
+
+    }
 }
--- core/src/com/mygdx/game/Service_Counter.java
+++ core/src/com/mygdx/game/Service_Counter.java
@@ -1,4 +1,14 @@
 package com.mygdx.game;
 
-public class Service_Counter {
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Service_Counter extends InteractivetileObject{
+
+    public Service_Counter(World world, TiledMap map, MapObject bounds){
+        super(world, map, bounds);
+        fixture.setUserData(this);
+
+    }
 }
