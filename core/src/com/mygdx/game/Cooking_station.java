282cc736951c1eef45170dca99564faddbf421f5
jguan924 <117920007+jguan924@users.noreply.github.com>
2023-01-25 21:48:34 +0000

    cooking station/stove and counter will accept items
    all stations now check what item you give it and there is now a progress bar to show how processed the item is

--- core/src/com/mygdx/game/Cooking_station.java
+++ core/src/com/mygdx/game/Cooking_station.java
@@ -1,14 +1,69 @@
 package com.mygdx.game;
 
+import Screens.Play_Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.physics.box2d.World;
 
+import java.util.Arrays;
+
 public class Cooking_station extends InteractivetileObject{
+    private float progress;
 
-    public Cooking_station(World world, TiledMap map, MapObject bounds){
-        super(world, map, bounds);
+    public Cooking_station(World world, TiledMap map, MapObject object){
+        super(world, map, object);
         fixture.setUserData(this);
+        AcceptableItems = new String[]{"E_Patty","E_Bun"};
+        progress = 10;
+    }
 
+    public void takeItem(foodItems item){
+        if (item!=null){
+            currentItem = item;
+            System.out.println("cooking");
+            Play_Screen.activeStations.add(this);
+        }
+    }
+
+    public void removeItem(Player chef){
+        chef.inventory.addItem(currentItem);
+        Play_Screen.activeStations.remove(this);
+        currentItem = null;
+        progress = 10;
+    }
+
+    public void progress(float dt){
+        progress -= dt;
+        if (progress<0){
+            if (currentItem.stage!=2){
+                currentItem.nextStage();
+                progress = 10;
+            }
+        }
+    }
+
+    public boolean isProgressable(){
+        if (progress>0){
+            if (currentItem.stage != 2){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void drawProgress(Batch batch, OrthographicCamera cam){
+        DrawProgressBar(cam, progress);
+
+        batch.begin();
+        batch.draw(currentItem.getItemSprite(),bounds.getX(),bounds.getY());
+        batch.end();
+    }
+
+    public boolean acceptableItem(String item){
+        if (Arrays.asList(AcceptableItems).contains(item)){return true;}
+
+        return false;
     }
 }
--- core/src/com/mygdx/game/Counters.java
+++ core/src/com/mygdx/game/Counters.java
@@ -1,13 +1,38 @@
 package com.mygdx.game;
 
+import Screens.Play_Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.physics.box2d.World;
 
-public class Counters extends InteractivetileObject{
-    public Counters(World world, TiledMap map, MapObject bounds){
-        super(world, map, bounds);
-        fixture.setUserData(this);
+import java.util.Arrays;
 
+public class Counters extends InteractivetileObject{
+    public Counters(World world, TiledMap map, MapObject object){
+        super(world, map, object);
+        fixture.setUserData(this);
     }
+
+    public void takeItem(foodItems item){
+        if (item!=null){
+            currentItem = item;
+            System.out.println("cooking");
+            Play_Screen.activeStations.add(this);
+        }
+    }
+
+    public void removeItem(Player chef){
+        chef.inventory.addItem(currentItem);
+        Play_Screen.activeStations.remove(this);
+        currentItem = null;
+    }
+
+    public void drawProgress(Batch batch, OrthographicCamera cam){
+        batch.begin();
+        batch.draw(currentItem.getItemSprite(),bounds.getX(),bounds.getY());
+        batch.end();
+    }
+
 }
