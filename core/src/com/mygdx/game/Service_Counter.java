8937865374fb7642ce2ba3fbe9c394ba6f188ff0
KHJH988 <98236360+KHJH988@users.noreply.github.com>
2023-01-26 23:35:28 +0000

    check orders method in the service counter  class

--- core/src/com/mygdx/game/Service_Counter.java
+++ core/src/com/mygdx/game/Service_Counter.java
@@ -4,11 +4,17 @@ import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.physics.box2d.World;
 
+import java.util.Stack;
+
 public class Service_Counter extends InteractivetileObject{
 
     public Service_Counter(World world, TiledMap map, MapObject bounds){
         super(world, map, bounds);
         fixture.setUserData(this);
+
+    }
+
+    public void CheckOrders(Stack Food){
 
     }
 }
